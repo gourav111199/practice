033f2355fcea82c002a2205b72f54db63ae89c2e
gourav111199 <gouravchouhan025@gmail.com>
2022-09-24 13:31:09 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React,{useState,useReducer} from 'react'
 import './App.css';
+// import { joke } from './Joke';
 
+const value = 5
+const reducer = (state,action) => {
+  if(action.type==="increment"){
+    return state+1    
+  }else{
+    if(state===0){
+     return  state=0
+    }else{
+      return state-1
+    }
+  }
+}
 function App() {
+  
+  const [state, dispatch] = useReducer(reducer, value)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <div>
+    <p>{state}</p>
+   
+    <button onClick={()=>dispatch({type:"increment"})}>INC</button>
+    <button onClick={()=>dispatch({type:"decrement"})}>DEC</button>
+
+
+    {/* <h1>{joke.joke}</h1> */}
+    {/* <button onClick={change} style={{cursor:"pointer"}}>Next</button> */}
+  </div>
+  </>
   );
 }
 
---
+++ src/Joke.js
@@ -0,0 +1,17 @@
+export const joke ={
+    "error": false,
+    "category": "Programming",
+    "type": "single",
+    "joke": "\"Honey, go to the store and buy some eggs.\"\n\"OK.\"\n\"Oh and while you're there, get some milk.\"\nHe never returned.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 18,
+    "safe": true,
+    "lang": "en"
+  }
